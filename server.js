bd64630d2b7e5a59cd6f5559c8c43677de1d8de9
Rohit Bhatt <node.jsdev3872@gmail.com>
2020-11-10 12:22:37 +0530

    database connected

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
+connectDB();
+
 const port = 4000;
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.json({
-        'Message':'This is working'
-    });
+app.get("/", (req, res) => {
+  res.json({
+    Message: "This is working",
+  });
 });
 
-app.listen(port,(err)=>{
-    if(!err){
-        console.log(`Server started at port ${port}`);
-    }else{
-        console.log(err);
-    }
-})
+app.listen(port, (err) => {
+  if (!err) {
+    console.log(`Server started at port ${port}`);
+  } else {
+    console.log(err);
+  }
+});
