b818a8c775b4dd5e605fd0391cc8855539691acd
dev3872 <45680062+dev3872@users.noreply.github.com>
2020-12-15 02:45:11 +0530

    make it better1

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = function (req, res, next) {
 
   // Verify token
   try {
-    jwt.verify(token, config.get("jwtSecret"), (error, decoded) => {
+    jwt.verify(token, process.env.jwtSecret, (error, decoded) => {
       if (error) {
         return res.status(401).json({ msg: "Token is not valid" });
       } else {
